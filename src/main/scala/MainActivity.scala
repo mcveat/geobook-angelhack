f4a9539d44c2fc7f6f4325f9ff4ae438eff4f396
Piotr Adamski <mcveat@gmail.com>
2013-05-04 13:38:39 +0200

    notification with button displayed

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -1,13 +1,32 @@
 package ah.locbook
 
-import _root_.android.app.Activity
+import android.app.{Notification, NotificationManager, Activity}
 import _root_.android.os.Bundle
+import android.content.Context._
+import android.widget.RemoteViews
+
+object MainActivity {
+  val NOTIFICATION_ID = 1
+}
 
 class MainActivity extends Activity with TypedActivity {
+  import MainActivity._
+
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
-    setContentView(R.layout.main)
+    val notifications = getSystemService(NOTIFICATION_SERVICE).asInstanceOf[NotificationManager]
+    notifications.notify(NOTIFICATION_ID, getNotification)
 
+    setContentView(R.layout.main)
     findView(TR.textview).setText("hello, world!")
+  }
+
+  private def getNotification = {
+    new Notification.Builder(this)
+      .setSmallIcon(android.R.drawable.ic_menu_directions)
+      .setContent(new RemoteViews(getPackageName, R.layout.notification))
+      .setAutoCancel(false)
+      .setOngoing(true)
+      .getNotification
   }
 }
